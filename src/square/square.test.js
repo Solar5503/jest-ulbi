b3748c41fd464d6400bf045ff48b1e0b0a215e35
Solar5503 <spidervvv@mail.ru>
2023-12-06 17:02:03 +0300

    added async functions tests

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -10,14 +10,23 @@ describe('square', () => {
   //before all tests
   beforeAll(() => {})
   test('Correct value', () => {
-    expect(square(2)).toBe(4)
-    expect(square(2)).toBeLessThan(5)
-    expect(square(2)).toBeGreaterThan(3)
-    expect(square(2)).not.toBeUndefined()
+    // expect(square(2)).toBe(4)
+    // expect(square(2)).toBeLessThan(5)
+    // expect(square(2)).toBeGreaterThan(3)
+    // expect(square(2)).not.toBeUndefined()
+    const spyMathPow = jest.spyOn(Math, 'pow')
+    square(2)
+    expect(spyMathPow).toHaveBeenCalledTimes(1)
+  })
+  test('Correct value equal to 1', () => {
+    const spyMathPow = jest.spyOn(Math, 'pow')
+    square(1)
+    expect(spyMathPow).toHaveBeenCalledTimes(0)
   })
 
   //after each test
   afterEach(() => {
+    jest.clearAllMocks()
     //delete user from bd
   })
   //after all tests
